85d1923703057ff661d653c3e8dc98d3429defdd
sudatra <mail2sudatra@gmail.com>
2025-09-09 20:14:07 +0530

    added supabase edge function to get route

---
+++ supabase/functions/routes/index.ts
@@ -0,0 +1,49 @@
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+Deno.serve(async (req) => {
+  const { 
+    origin, 
+    destination 
+  }: { 
+    origin: Coordinates; 
+    destination: Coordinates 
+  } = await req.json();
+
+  const response = await fetch(
+    `https://routes.googleapis.com/directions/v2:computeRoutes?key=${Deno.env.get('GOOGLE_MAPS_API_KEY')}`,
+    {
+      method: 'POST',
+      'Content-Type': 'application/json',
+      "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline,routes.legs.polyline"
+    },
+    body: JSON.stringify(
+      {
+        origin: { location: { latLng: origin } },
+        destination: { location: { latLng: destination } },
+        travelMode: 'DRIVE',
+        polylineEncoding: "GEO_JSON_LINESTRING"
+      }
+    )
+  );
+
+  if(!response.ok) {
+    const error = await response.json();
+    console.error(error);
+
+    throw new Error('Http Error');  
+  }
+
+  const data = await response.json();
+  const res = data.routes[0];
+  return new Response(
+    JSON.stringify(res),
+    { headers: { "Content-Type": "application/json" } },
+  )
+});

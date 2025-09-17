6ed25e52dd0ffda75fb0eeb03c007c522a51f174
sudatra <mail2sudatra@gmail.com>
2025-09-17 19:46:44 +0530

    added fare estimation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:duration/duration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
@@ -40,6 +42,7 @@ class _MainAppState extends State<MainApp> {
   final Set <Polyline> _polylines = {};
   final Set <Marker> _markers = {};
   BitmapDescriptor? _pinIcon;
+  late int _fare;
 
   @override
   void initState() {
@@ -157,84 +160,102 @@ class _MainAppState extends State<MainApp> {
         bottomSheet: _appState == AppState.confirmFare
           ? Container(
             width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16).copyWith(bottom: MediaQuery.of(context).padding.bottom),
             decoration: const BoxDecoration(
               color: Colors.white,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Text('Confirm Fare'),
-                SizedBox(height: 16),
+                Text(
+                  'Confirm Fare',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 16),
 
-                Text('Estimated Fare: ')
+                Text('Estimated Fare: ${NumberFormat.currency(
+                  symbol: '\$',
+                  decimalDigits: 2
+                ).format(_fare / 100)}'),
+                const SizedBox(height: 16),
+
+                ElevatedButton(
+                  onPressed: () async {}, 
+                  child: const Text('Confirm Fare')
+                )
               ],
             ),
           )
           : SizedBox.shrink()
         ,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () async {
-            final response = await supabase.functions.invoke(
-              'routes',
-              body: {
-                'origin': {
-                  'latitude': _currentLocation!.latitude,
-                  'longitude': _currentLocation!.longitude
-                },
-                'destination': {
-                  'latitude': _selectedDestination!.latitude,
-                  'longitude': _selectedDestination!.longitude
+        floatingActionButton: _appState == AppState.choosingLocation 
+          ? FloatingActionButton.extended(
+            onPressed: () async {
+              final response = await supabase.functions.invoke(
+                'routes',
+                body: {
+                  'origin': {
+                    'latitude': _currentLocation!.latitude,
+                    'longitude': _currentLocation!.longitude
+                  },
+                  'destination': {
+                    'latitude': _selectedDestination!.latitude,
+                    'longitude': _selectedDestination!.longitude
+                  }
                 }
-              }
-            );
+              );
 
-            final data = response.data as Map <String, dynamic>;
-            final coordinates = data['legs'][0]['polyline']['geoJsonLinestring']['coordinates'] as List <dynamic>;
-            final duration = data['duration'] as String;
+              final data = response.data as Map <String, dynamic>;
+              final coordinates = data['legs'][0]['polyline']['geoJsonLinestring']['coordinates'] as List <dynamic>;
+              final duration = parseDuration(data['duration'] as String);
+              _fare = (duration.inMinutes * 40).ceil();
 
-            final polylineCoordinates = coordinates.map((coordinate) {
-              return LatLng(coordinate[1], coordinate[0]);
-            }).toList();
+              final polylineCoordinates = coordinates.map((coordinate) {
+                return LatLng(coordinate[1], coordinate[0]);
+              }).toList();
 
-            setState(() {
-              _polylines.add(
-                Polyline(
-                  polylineId: const PolylineId('route'),
-                  points: polylineCoordinates,
-                  color: Colors.black,
-                  width: 5
+              setState(() {
+                _polylines.add(
+                  Polyline(
+                    polylineId: const PolylineId('route'),
+                    points: polylineCoordinates,
+                    color: Colors.black,
+                    width: 5
+                  )
+                );
+              });
+
+              _markers.add(
+                Marker(
+                  markerId: MarkerId('destination'),
+                  position: _selectedDestination!,
+                  icon: _pinIcon!
                 )
               );
-            });
 
-            _markers.add(
-              Marker(
-                markerId: MarkerId('destination'),
-                position: _selectedDestination!,
-                icon: _pinIcon!
-              )
-            );
+              final bounds = LatLngBounds(
+                southwest: LatLng(
+                  polylineCoordinates.map((e) => e.latitude).reduce((a, b) => a < b ? a : b),
+                  polylineCoordinates.map((e) => e.longitude).reduce((a, b) => a < b ? a : b)
+                ),
+                northeast: LatLng(
+                  polylineCoordinates.map((e) => e.latitude).reduce((a, b) => a > b ? a : b),
+                  polylineCoordinates.map((e) => e.longitude).reduce((a, b) => a > b ? a : b)
+                )
+              );
 
-            final bounds = LatLngBounds(
-              southwest: LatLng(
-                polylineCoordinates.map((e) => e.latitude).reduce((a, b) => a < b ? a : b),
-                polylineCoordinates.map((e) => e.longitude).reduce((a, b) => a < b ? a : b)
-              ),
-              northeast: LatLng(
-                polylineCoordinates.map((e) => e.latitude).reduce((a, b) => a > b ? a : b),
-                polylineCoordinates.map((e) => e.longitude).reduce((a, b) => a > b ? a : b)
-              )
-            );
+              _mapController.animateCamera(
+                CameraUpdate.newLatLngBounds(bounds, 100)
+              );
 
-            _mapController.animateCamera(
-              CameraUpdate.newLatLngBounds(bounds, 100)
-            );
-
-            _goToNextState();
-          }, 
-          label: const Text('Confirm Destination')
-        ),
+              _goToNextState();
+            }, 
+            label: const Text('Confirm Destination')
+          )
+          : SizedBox.shrink()
+        ,
       ),
     );
   }
